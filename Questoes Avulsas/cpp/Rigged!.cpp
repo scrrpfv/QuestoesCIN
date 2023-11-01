250073725cdef537a6a6b4e70c19e57183b15a48
scrrpfv <149007261+scrrpfv@users.noreply.github.com>
2023-11-01 17:05:01 -0300

    Update and rename nada.cpp to Rigged!.cpp

---
+++ Questoes Avulsas/cpp/Rigged!.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int num_testes;
+    cin >> num_testes;
+    for (int i=0; i<num_testes; i++)
+    {
+        int peso = -1;
+        int num_participantes;
+
+        cin >> num_participantes;
+
+        int forçapoly;
+        int resistênciapoly;
+
+        cin >> forçapoly;
+        cin >> resistênciapoly;
+
+        for (int i=1; i<num_participantes; i++)
+        {   
+            int força;
+            int resistência;
+            
+            cin >> força >> resistência;
+        }
+    }
+}
