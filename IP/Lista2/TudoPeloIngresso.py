8b3c67597e078ebe91a1d338591a3178cfd1ce05
afp2 <afp2@g5c36.cin.ufpe.br>
2023-11-27 14:54:27 -0300

    .

--- IP/Lista2/TudoPeloIngresso.py
+++ IP/Lista2/TudoPeloIngresso.py
@@ -5,17 +5,20 @@ nome = input()
 amigos = 0
 ingressos_validos = 0
 posicao_minima = int(tempo_minutos*16/10)
+posicao_escolhida = posicao_minima
 while nome != 'end':
     if nome != 'laura' and nome != 'carlos' and nome != 'roberto' and amigos <= computadores_disponiveis:
         amigos += 1
+    nome = input()
 
 if not amigos:
     print('Ah não! João não conseguiu nenhum amigo que o ajudasse. Agora ele vai ter que contar com a sorte para pegar um bom lugar na fila, usando apenas seu computador.')
 else:
     print(f'Bom começo! Consegui {amigos} amigos que podem me ajudar a comprar o ingresso')
 for i in range(amigos):
+    conseguiu_ingresso = False
     valor_do_ingresso = ''
-    while valor_do_ingresso != 'end':
+    while valor_do_ingresso != 'end' and not conseguiu_ingresso:
         valor_do_ingresso = int(input())
         local_do_show = input()
         if local_do_show == 'rio de janeiro' or local_do_show == 'são paulo' or local_do_show == 'buenos aires':
@@ -25,11 +28,13 @@ for i in range(amigos):
                 posicao_fila = int(input())
                 if posicao_fila < posicao_minima:
                     print('ISSOOO, conseguimos um ingresso!!!')
-                    computador_escolhido = i
-                    posicao_minima = posicao_fila
+                    conseguiu_ingresso = True
                     ingressos_validos += 1
+                    if posicao_fila < posicao_escolhida:
+                        computador_escolhido = i
+                        posicao_escolhida = posicao_fila
                 else:
-                    print(f'Caramba, essa foi quase! Mas infelizmente não consegui um bom lugar na fila dos ingressos no computador de número {i}')
+                    print(f'Caramba, essa foi quase! Mas infelizmente não consegui um bom lugar na fila dos ingressos no computador de número {i + 1}')
             else:
                 print('Caramba! Só tinha ingresso para a pista vip, que está bem acima do meu orçamento.')
 
