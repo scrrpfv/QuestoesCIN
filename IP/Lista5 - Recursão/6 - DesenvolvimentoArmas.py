da8ac4bde756120edf33195791cad6d997d0e083
scrrpfv <149007261+scrrpfv@users.noreply.github.com>
2024-02-05 14:56:55 -0300

    Update 6 - DesenvolvimentoArmas.py

--- IP/Lista5 - Recursão/6 - DesenvolvimentoArmas.py
+++ IP/Lista5 - Recursão/6 - DesenvolvimentoArmas.py
@@ -1,7 +1,6 @@
 x1 = float(input())
 a, b, c, d, e = float(input()), float(input()), float(input()), float(input()), float(input())
 h = 1e-9
-zero = 0.001
 
 
 def processa(x1, equacao=''):
@@ -10,24 +9,22 @@ def processa(x1, equacao=''):
     return 0
   else:
     y1 = (a*(x1+b)**c)+(e*x1)+d
-    
-    if abs(y1) < zero:
+    if round(y1, 3) == 0:
       print(f'Acertou em cheio! A raiz foi encontrada e o valor dela nas abscissas é aproximadamente {round(x1, 3)}, e a equação da reta tangente neste ponto é aproximadamente {equacao}.')
       return 0
-      
     resultado_derivada = (((a*((x1+h)+b)**c)+(e*(x1+h))+d) - ((a*(x1+b)**c)+(e*x1)+d)) / h
     termo_independente = y1-(resultado_derivada*x1)
-    sinal = '+'*(termo_independente > 0) + '-'*(termo_independente < 0)
-    if abs(resultado_derivada) > zero:
+    sinal = '+'*(termo_independente > 0) + ''*(termo_independente < 0)
+    if abs(round(resultado_derivada, 3)) > 0:
       equacao = (f'y={round(resultado_derivada, 3)}*x{sinal}{round(termo_independente, 3)}')
       x2 = (-termo_independente)/resultado_derivada
       y2 = (a*(x2+b)**c)+(e*x2)+d
     else:
       equacao = (f'y={round(termo_independente, 3)}')
     y = resultado_derivada*x1+termo_independente
-    if abs(y1) > zero:
+    if abs(round(y1, 3)) > 0:
       print(f'Encontramos o ponto ({round(x1, 3)};{round(y1, 3)}) da função, a equação da reta tangente deste ponto é aproximadamente {equacao}, porém, ainda não é a raiz devemos continuar para otimizar a trajetória do disparo.')
-    if abs(resultado_derivada) < zero:
+    if round(resultado_derivada, 3) == 0:
       print(f'Droga, a reta tangente {equacao} é paralela ao eixo das abscissas, não tem raiz, tenho pena de quem estiver usando a arma quando isto acontecer, kkkkk.')
       return 0
     processa(x2, equacao)
