2407694e712b9683b92b173162becdd0cd5183a9
imgs <imgs@g5c06.cin.ufpe.br>
2024-04-29 16:20:48 -0300

    coisa

--- AED/Provas/Prova1/q1.cpp
+++ AED/Provas/Prova1/q1.cpp
@@ -2,11 +2,13 @@
 
 using namespace std;
 
+int n_ins;
+
 void merge(int *arr, int l, int r, int *idx)
 {
     int m = (l + r) / 2;
-    int temp[r];
-    int tempidx[r];
+    int temp[n_ins];
+    int tempidx[n_ins];
     for(int i=0; i<=r; i++)
     {
         temp[i] = arr[i];
@@ -16,25 +18,25 @@ void merge(int *arr, int l, int r, int *idx)
     int i2 = m + 1;
     for(int curr=l; curr<=r; curr++)
     {
-        if(i2 == m + 1)
+        if(i1 == m + 1)
         {
-            arr[curr] = temp[i2++];
-            idx[curr] = tempidx[i2+1];
+            arr[curr] = temp[i2];
+            idx[curr] = tempidx[i2++];
         }
         else if(i2 > r)
         {
-            arr[curr] = temp[i1++];
-            idx[curr] = tempidx[i1+1];
+            arr[curr] = temp[i1];
+            idx[curr] = tempidx[i1++];
         }
-        else if(temp[i1] <= temp[i2])
+        else if(arr[tempidx[i1]] >= arr[tempidx[i2]]) 
         {
-            arr[curr] = temp[i1++];
-            idx[curr] = tempidx[i1+1];
+            arr[curr] = temp[i1];
+            idx[curr] = tempidx[i1++];
         }
         else
         {
-            arr[curr] = temp[i2++];
-            idx[curr] = tempidx[i2+1];
+            arr[curr] = temp[i2];
+            idx[curr] = tempidx[i2++];
         }
     }
 }
@@ -59,7 +61,6 @@ int main()
     cin >> c;
     for(int i=1; i<=c; i++)
     {
-        int n_ins;
         int n_vag;
         cin >> n_ins >> n_vag;
 
@@ -76,24 +77,25 @@ int main()
             idx[j] = j;
         }
 
-        for(int j=0; j<n_vag; j++)
-        {
-            cout << notas[j];
-        }
-
         mergesort(idades, 0, n_ins-1, idx);
+        
         mergesort(notas, 0, n_ins-1, idx);
+        
+        // cout << "sortou\n";
+        //for(int j=0; j<n_vag; j++)
+        //{
+        //    cout << notas[j] << "\n";
+        //}
 
-        for(int j=0; j<n_vag; j++)
+        for(int j=0; j<min(n_vag, n_ins); j++)
         {
-            cout << notas[j];
+            cout << nomes[idx[j]] << "\n";
+            //cout << notas[j] << "\n";
         }
-
-        for(int j=0; j<n_vag; j++)
-        {
-            cout << nomes[idx[j]];
-            cout << notas[j];
+        for (int i = 0; i < n_vag-n_ins; i++) {
+            cout << 'x' << '\n';
         }
+        
     }
 
     return 0;
