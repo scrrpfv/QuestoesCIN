2670aa30f21cf0a7ffb8a2f5cf2b18c3cf5c6e81
afp2 <afp2>
2024-11-19 10:00:03 -0300

    ta muita coisa mas e tudo mentira

---
+++ Algoritmos_Genéricos/operacoes_matriz.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int arr1[2][3] = {{1,0,3},{4,6,1}};
+    int arr2[2][3] = {{5,3,1},{0,1,7}};
+
+    return 0;
+}
--- Algoritmos_Genéricos/operacoes_vetor.cpp
+++ Algoritmos_Genéricos/operacoes_vetor.cpp
@@ -1,4 +1,6 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 int arr_sum(int *arr, int n) {
     int sum = 0;
@@ -26,3 +28,22 @@ int dot_product(int *arr1, int *arr2, int n) {
     return p;
 }
 
+int gt(int v1, int v2) {
+    return v1 > v2;
+}
+
+int main() {
+
+    int arr1[4] = {5,3,2,4};
+    int arr2[6] = {0,1,2,8,3};
+
+    int sum = arr_sum(arr1, 4);
+    int max = arr_most(arr2, 6, gt);
+    int dot = dot_product(arr1, arr2, 4); // will use arr2 just up to position 4
+
+    cout << sum << "\n";
+    cout << max << "\n";
+    cout << dot << "\n";
+
+    return 0;
+}
