b927de8e9cdc34348948aca1f6944a8954715e28
afp2 <afp2>
2024-12-03 11:34:25 -0300

    questao desafio sendo resolvida

--- AED/Questões_Desafio/q1/q1.cpp
+++ AED/Questões_Desafio/q1/q1.cpp
@@ -2,28 +2,20 @@
 
 using namespace std;
 
-typedef struct iLink {
-    iLink* next;
-    iLink* prev;
-    int element;
-} iLink;
+typedef struct Link {
+    Link* next;
+    Link* prev;
+    int is_op;
+    char op;
+    int value;
+} Link;
 
 typedef struct List {
-    iLink* head;
-    iLink* tail;
-    iLink* curr;
+    Link* head;
+    Link* tail;
+    Link* curr;
     int size;
 } List;
-
-typedef struct sLink {
-    sLink* next;
-    char op;
-} sLink;
-
-typedef struct Stack {
-    sLink* top;
-    int size;
-} Stack;
 
 void next(List* l) {
     l->curr = l->curr->next;
@@ -33,30 +25,22 @@ void prev(List* l) {
     l->curr = l->curr->prev;
 }
 
-void remove_curr(List* l) {
-    Link* to_remove = l->curr;
-
-    if (l->head == l->tail) {
-        l->head = l->tail = l->curr = NULL;
+void remove(List* l, Link* x) {
+    if (x->prev != NULL) {
+        x->prev->next = x->next;
+        x->next->next->prev = x->next;
     }
     else {
-        if (l->curr == l->head) {
-            l->head = l->curr->next;
-            l->head->prev = NULL;
-        }
-        else if (l->curr == l->tail) {
-            l->tail = l->curr->prev;
-            l->tail->next = NULL;
-        }
-        else {
-            l->curr->prev->next = l->curr->next;
-            l->curr->next->prev = l->curr->prev;
-        }
+        l->tail = x->next;
     }
+    if (x->next != NULL) {
+        x->next->prev = x->next;
+    }
+    l->size--;
 }
 
 List* create_list() {
-    List* l = (List*)malloc(sizeof(List));
+    List* l = new List;
     l->curr = NULL;
     l->head = NULL;
     l->tail = NULL;
@@ -64,12 +48,13 @@ List* create_list() {
     return l;
 }
 
-Link* create_link(char op, int element) {
-    Link* k = (Link*)malloc(sizeof(Link));
+Link* create_link(int is_op, char op, int value) {
+    Link* k = new Link;
     k->next = NULL;
     k->prev = NULL;
-    k->element = element;
+    k->is_op = is_op;
     k->op = op;
+    k->value = value;
     return k;
 }
 
@@ -95,9 +80,53 @@ void insert_link(List* l, Link* k) {
     l->size++;
 }
 
+typedef struct Stack {
+    Link* top;
+    int size;
+} Stack;
+
+Stack* create_stack () {
+    Stack* s = new Stack;
+    s->size = 0;
+    s->top = NULL;
+    return s;
+}
+
+void push(Stack* s, Link* l) {
+    l->prev = s->top;
+    s->top = l;
+    s->size++;
+}
+
+Link* pop(Stack* s) {
+    Link* p = s->top;
+    s->top = s->top->prev;
+    s->size--;
+}
+
 int main() {
     string str;
     cin >> str;
 
+    Stack* s = create_stack();
+    List* l = create_list();
 
+    for (char& c : str) {
+        if (c != '(') {
+            if (c == '+' | c == '-' | c == '/' | c == '*' | c == '^') {
+                Link* x = create_link(1, c, 0);
+                push(s, x);
+            }
+            else if (c == ')') {
+                l->curr = l->head;
+                insert_link(l, pop(s));
+            }
+            else {
+                l->curr = l->head;
+                int ic = c - '0';
+                Link* x = create_link(0, ' ', ic);
+                insert_link(l, x);
+            }
+        }
+    }
 }
