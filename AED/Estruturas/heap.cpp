773a506934bca773e3beab9df7860f2d9afdf3d0
afp2 <149007261+scrrpfv@users.noreply.github.com>
2025-02-17 20:24:26 -0300

    Update heap.cpp
    Agora sim esse heap

--- AED/Estruturas/heap.cpp
+++ AED/Estruturas/heap.cpp
@@ -1,25 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
 typedef struct heap {
     int* arr;
     int max_size;
     int size;
 } heap;
 
-void insert_topdown(heap* h, int v) {
-    int k = 1;
-    bool heap = false;
-    while (!heap) {
-        int j = 2*k;
-        if (j < h->size) {
-            if (h->arr[j] < h->arr[j+1]) {
-                j++;
-            }
-        }
-        if (v >= h->arr[j]) {
-            heap = true;
-        } else {
-            h->arr[k] = h->arr[j];
-            k = j;
-        }
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void heapify(heap* h, int i) {
+    int l = 2*i + 1;
+    int r = 2*i + 2;
+    int maior;
+    if (l < h->size && h->arr[l] > h->arr[i]) {
+        maior = l;
+    } else {
+        maior = i;
     }
-    h->arr[k] = v;
+    if (r < h->size && h->arr[r] > h->arr[maior]) {
+        maior = r;
+    }
+    if (maior != i) {
+        swap(h->arr[i], h->arr[maior]);
+        heapify(h, maior);
+    }
+}
+
+void build_heap(heap *h) {
+    for (int i = h->size / 2; i >= 0; i--) {
+        heapify(h, i);
+    }
+}
+
+void insert_heap(heap* h, int v) {
+    h->arr[h->size] = v;
+    h->size++;
+    int i = h->size;
+    while (i > 0 && h->arr[i - 1] > h->arr[i/2 - 1]) {
+        swap(h->arr[i - 1], h->arr[i/2 - 1]);
+        i = i/2;
+    }
+}
+
+int main() {
+    heap* h = new heap;
+    int arr[10] = {10, 4, 8, 7, 2, 1, 30, 25, 19, 17};
+    h->arr = arr;
+    h->max_size = 10;
+    h->size = 10;
+    
+    build_heap(h);
+    
+    cout << "heap: ";
+    for (int i = 0; i < h->size; i++) {
+        cout << h->arr[i] << " ";
+    }
+    cout << "\n";
 }
